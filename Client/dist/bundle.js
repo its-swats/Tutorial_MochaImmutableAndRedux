287e4a6533bd49624ee3a48d84655cd944614b7c
Shawn Watson <shawnw86@gmail.com>
2016-01-30 15:03:30 -0800

    Add tests for react voting components

--- Client/dist/bundle.js
+++ Client/dist/bundle.js
@@ -54,7 +54,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "057c8f4951da000d3386"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "b84d4e417d564c8c5dab"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
@@ -8041,7 +8041,7 @@
 
 	var pair = ['Batman', 'Hackers'];
 
-	_reactDom2.default.render(_react2.default.createElement(_Voting2.default, { pair: pair }), document.getElementById('#app'));
+	_reactDom2.default.render(_react2.default.createElement(_Voting2.default, { pair: pair, hasVoted: 'Batman' }), document.getElementById('app'));
 
 	/* REACT HOT LOADER */ }).call(this); } finally { if (true) { (function () { var foundReactClasses = module.hot.data && module.hot.data.foundReactClasses || false; if (module.exports && module.makeHot) { var makeExportsHot = __webpack_require__(245); if (makeExportsHot(module, __webpack_require__(139))) { foundReactClasses = true; } var shouldAcceptModule = true && foundReactClasses; if (shouldAcceptModule) { module.hot.accept(function (err) { if (err) { console.error("Cannot not apply hot update to " + "index.jsx" + ": " + err.message); } }); } } module.hot.dispose(function (data) { data.makeHot = module.makeHot; data.foundReactClasses = foundReactClasses; }); })(); } }
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(4)(module)))
@@ -27970,7 +27970,7 @@
 
 	/* WEBPACK VAR INJECTION */(function(module) {/* REACT HOT LOADER */ if (true) { (function () { var ReactHotAPI = __webpack_require__(77), RootInstanceProvider = __webpack_require__(85), ReactMount = __webpack_require__(87), React = __webpack_require__(139); module.makeHot = module.hot.data ? module.hot.data.makeHot : ReactHotAPI(function () { return RootInstanceProvider.getRootInstances(ReactMount); }, React); })(); } try { (function () {
 
-	"use strict";
+	'use strict';
 
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
@@ -27983,25 +27983,39 @@
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	exports.default = _react2.default.createClass({
-	  displayName: "Voting",
+	  displayName: 'Voting',
 
 	  getPair: function getPair() {
 	    return this.props.pair || [];
 	  },
-
+	  isDisabled: function isDisabled() {
+	    return !!this.props.hasVoted;
+	  },
+	  hasVotedFor: function hasVotedFor() {
+	    return this.props.hasVoted === entry;
+	  },
 	  render: function render() {
+	    var _this = this;
+
 	    return _react2.default.createElement(
-	      "div",
-	      { className: "voting" },
+	      'div',
+	      { className: 'voting' },
 	      this.getPair().map(function (entry) {
 	        return _react2.default.createElement(
-	          "button",
-	          { key: entry },
+	          'button',
+	          { key: entry, onClick: function onClick() {
+	              return _this.props.vote(entry);
+	            }, disabled: _this.isDisabled() },
 	          _react2.default.createElement(
-	            "h1",
+	            'h1',
 	            null,
 	            entry
-	          )
+	          ),
+	          _this.hasVotedFor ? _react2.default.createElement(
+	            'div',
+	            { className: 'label' },
+	            'Voted'
+	          ) : null
 	        );
 	      })
 	    );
--- Client/test/voting_spec.jsx
+++ Client/test/voting_spec.jsx
@@ -2,7 +2,7 @@ import React from 'react/addons';
 import Voting from '../src/components/Voting';
 import {expect} from 'chai';
 
-const {renderIntoDocument, scryRenderedDOMComponentsWithTag} = React.addons.TestUtils;
+const {renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate} = React.addons.TestUtils;
 
 describe ('voting', () => {
   it('renders in to document', () => {
@@ -13,5 +13,25 @@ describe ('voting', () => {
     expect(buttons.length).to.equal(2);
     expect(buttons[0].textContent).to.equal('Batman');
     expect(buttons[1].textContent).to.equal('Hackers');
+  });
+
+  it('invokes a callback when a button is clicked', () => {
+    let votedWith;
+    const vote = (entry) => votedWith = entry;
+    const component = renderIntoDocument(
+      <Voting pair={['Batman', 'Hackers']} vote={vote} />
+    )
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    Simulate.click(buttons[0]);
+    expect(votedWith).to.equal('Batman');
+  });
+
+  it('disables voting buttons after voting', () => {
+    const component = renderIntoDocument(
+      <Voting pair={['Batman', 'Hackers']} hasVoted='Hackers' />
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    expect(buttons[0].hasAttribute('disabled')).to.equal(true);
+    expect(buttons[1].hasAttribute('disabled')).to.equal(true);
   });
 });
